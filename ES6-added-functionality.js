fccda53793e58003d744f4eb0b47235a5d459c6d
Ben Hunter <learntocode@benhunter.me>
2021-10-15 11:45:49 -0500

    ES6 Features

--- ES6-added-functionality.js
+++ ES6-added-functionality.js
@@ -29,3 +29,76 @@ const exampleArray = [1, 2, 3];
 for (const item of exampleArray) {
     console.log(item);
 }
+
+
+// Spread operator
+// Arrays
+
+const originalArray = [1, 2, 3];
+// const copyArray = originalArray.concat([]);
+const spreadArray = [...originalArray, 4, 5, 6];
+console.log(spreadArray);
+
+function sumValues(a, b, c) {
+    return a + b + c;
+}
+
+console.log(sumValues(...originalArray));
+
+// Objects
+const originalObject = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+const copyObject = {...originalObject};
+
+const copyObjectWithNewValues = {...originalObject, a: 43};  // override a
+console.log(copyObjectWithNewValues);
+
+
+// Rest Parameters
+function sumValuesRest(...values) {
+    return values.reduce((acc, value) => acc + value);
+}
+console.log(sumValuesRest(1, 2, 3, 4, 5));
+
+const newAddFunction = (firstNum, ...nums) => {
+    return nums.reduce((acc, num) => acc + num);
+}
+console.log(newAddFunction(1, 2, 3, 4, 5));
+
+
+// Destructuring
+// from functions
+const bob = {
+    first: 'Bob',
+    last: 'Smith',
+    age: 30
+};
+const normalFunction = (obj) => {
+    return `${obj.first} ${obj.last}`;
+}
+console.log(normalFunction(bob));
+
+const destructuredFunction = ({first, last}, ...obj) => {
+    console.log(first, last, obj);
+    return `${first} ${last}`;
+};
+console.log(destructuredFunction(bob));
+
+// Destructured assignment from objects
+const {first, last} = bob;
+console.log(first, last);
+
+// Destructured assignment from arrays
+const [one, two] = [1, 2, 3];
+console.log(one, two);
+
+
+// Default parameters
+const defaultFunction = (a, b = 'default') => {
+    return a + b;
+}
+console.log(defaultFunction(1));
+
